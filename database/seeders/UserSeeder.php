6edaaf95121cc8992063ffd2317bfe7494fc7c4c
Infyne <infyne@Mac.lan>
2025-08-19 16:50:46 +0530

    new seed file

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,35 +14,8 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = [
-            [
-                'uuid' => 'SS-AD-0001',
-                'name' => 'Arpita Roy',
-                'email' => 'roya91455@gmail.com',
-                'phone' => '8794748689',
-                'password' => Hash::make('Pizza&Mountain$Clock7Lightning'),
-                'email_verified_at' => now(),
-            ],
-            [
-                'uuid' => 'SS-AD-0002',
-                'name' => 'Pinak Suklabaidya',
-                'email' => 'pinaksuklabaidya517@gmail.com',
-                'phone' => '6901979091',
-                'password' => Hash::make('Bill&PlantKitchenEngine1'),
-                'email_verified_at' => now(),
-            ],
-            [
-                'uuid' => 'SS-AD-0003',
-                'name' => 'Bishal Deb',
-                'email' => 'solution@infyne.in',
-                'phone' => '8099227246',
-                'password' => Hash::make('Yuc8$RikA34%ZoPPao98t'),
-                'email_verified_at' => now(),
-            ],
-        ];
-
-        foreach ($users as $userData) {
-            User::create($userData);
-        }
+        $this->call([
+            DatabaseSeeder::class,
+        ]);
     }
 }
